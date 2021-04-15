36ed3833f79775027acaf1f2077a238959b4bba9
Konstantin Gribov <grossws@gmail.com>
2021-04-15 03:42:47 +0300

    feat: Add private plugins bootstrap support

--- src/main/kotlin/ws/gross/gradle/NexusPlugin.kt
+++ src/main/kotlin/ws/gross/gradle/NexusPlugin.kt
@@ -16,12 +16,19 @@
 
 package ws.gross.gradle
 
+import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
+import org.gradle.api.logging.Logging
 import org.gradle.api.plugins.PluginInstantiationException
 import org.gradle.kotlin.dsl.*
 import org.gradle.util.GradleVersion
+import java.io.File
 import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.util.Properties
 
 class NexusPlugin : Plugin<Settings> {
   override fun apply(settings: Settings) {
@@ -38,6 +45,8 @@ internal class NexusPluginImpl : Plugin<Settings> {
     const val NEXUS_REPO_NAME = "nexus"
     const val GRADLE_RELEASES_REPO_NAME = "nexusReleasesGradle"
     const val GRADLE_SNAPSHOTS_REPO_NAME = "nexusSnapshotsGradle"
+
+    val logger = Logging.getLogger(NexusPlugin::class.java)
   }
 
   lateinit var baseUrl: String
@@ -48,6 +57,8 @@ internal class NexusPluginImpl : Plugin<Settings> {
 
     settings.configurePluginRepos()
     settings.configureRepos()
+
+    settings.addPrivatePluginsBootstrap()
   }
 
   private fun Settings.configurePluginRepos() {
@@ -95,6 +106,39 @@ internal class NexusPluginImpl : Plugin<Settings> {
     val nexusExclusive: String? by this
     val exclusive = nexusExclusive.parseSwitch(false)
     configureNexusRepo(nexusTarget ?: "public", groups, regexes, defaultRegex, exclusive)
+  }
+
+  private fun Settings.addPrivatePluginsBootstrap() {
+    val nexusPrivatePluginsBootstrap: String? by settings
+    nexusPrivatePluginsBootstrap ?: return
+
+    val components = nexusPrivatePluginsBootstrap!!.split(':')
+    require(components.size == 3) { "nexusPrivatePluginsBootstrap should be in group:module:version notation" }
+    val (group, module, version) = components
+
+    val httpClient = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build()
+
+    val cached = File("$settingsDir/.gradle/${group}.${module}-$version.properties")
+    if (!cached.exists()) {
+      val path = "${group.replace('.', '/')}/$module/$version/$module-$version.properties"
+      val req = HttpRequest.newBuilder(URI.create("$baseUrl/repository/gradle/$path")).GET().build()
+
+      val resp = httpClient.send(req, HttpResponse.BodyHandlers.ofFile(cached.toPath()))
+
+      if (resp.statusCode() != 200) {
+        throw GradleException("Failed to download ${resp.uri()}: $resp")
+      }
+    }
+
+    if (cached.exists()) {
+      val props = Properties().apply { cached.reader().use { load(it) } }
+      val ids = props.getProperty("ids").split(',').map { it.trim() }
+      val version = props.getProperty("version")
+
+      pluginManagement.plugins {
+        ids.forEach { id(it) version (version) }
+      }
+    }
   }
 
   @Suppress("UnstableApiUsage")
